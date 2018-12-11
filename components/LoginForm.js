66b33bb9f91f0b6f98dd9ea0ea15029c7230856f
Kittisak Pharaphan <kittisak.py@gmail.com>
2018-12-11 21:30:30 +0700

    form layout

---
+++ components/LoginForm.js
@@ -0,0 +1,34 @@
+class LoginForm extends React.Component {
+
+    state = {
+        email: '',
+        password: ''
+    }
+    
+    handleChange = (event) =>{
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(this.state)
+    }
+    
+    render () {
+
+
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+            <div><input type="email" name="email" placeholder="email" onChange={this.handleChange}/></div>
+            <div><input type="password" name="password" placeholder="password" onChange={this.handleChange}/></div>
+            <button type="submit">Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default LoginForm
