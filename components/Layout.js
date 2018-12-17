254fb3aced96884997679508fc8d5cccc5e32988
Kittisak Pharaphan <kittisak.py@gmail.com>
2018-12-17 20:53:21 +0700

    session play

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,27 @@
 import Link from 'next/link'
 
-const Layout = ({title, children}) => (
+const Layout = ({title, children, auth}) => {
+
+    const {user = {}} = auth || {}
+    
+    return (
     <div className="root">
         <nav className="navbar">
-            <span>Welcome, <strong>Guest</strong></span>
+            <span>Welcome, <strong>{user.name || "Guest"}</strong></span>
 
             <div>
                 <Link href="/"><a>Home</a></Link>
-                <Link href="/profile"><a>Profile</a></Link>
-                <button>Logout</button>
-                <Link href="/login"><a>Login</a></Link>
+                {user.email ? (
+                    // Auth Navigation
+                    <React.Fragment>
+                        <Link href="/profile"><a>Profile</a></Link>
+                        <button>Logout</button>
+                    </React.Fragment>
+                    ) : (
+                        // unauth Navigation
+                    <Link href="/login"><a>Login</a></Link>
+                    )
+                }
             </div>
         </nav>
 
@@ -41,6 +53,6 @@ const Layout = ({title, children}) => (
             }
         `}</style>
     </div>
-)
+)}
 
 export default Layout
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Layout from '../components/Layout'
 import Link from 'next/link'
+import {authInitialProps} from '../lib/auth'
 
-export default function Index() {
+export default function Index(props) {
     return (
-        <Layout title="Home">
+        <Layout title="Home" {...props}>
             <Link href="/profile"><a>Go to profile</a></Link>
         </Layout>
     )
 }
+
+Index.getInitialProps = authInitialProps()
